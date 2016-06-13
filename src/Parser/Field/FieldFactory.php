e9d012fc175e4ac8c8f541f0d7e49becb43d3c64
Aaron Valandra <avaland@woopta.com>
2016-06-13 10:38:24 -0500

    Started some work on parsing individual field types.

--- src/Parser/Field/FieldFactory.php
+++ src/Parser/Field/FieldFactory.php
@@ -4,17 +4,37 @@
 namespace Bolt\Extension\Bolt\JsonApi\Parser\Field;
 
 use Bolt\Extension\Bolt\JsonApi\Parser\Field\Type\Generic;
+use Bolt\Storage\Entity\Content;
 
 class FieldFactory
 {
+    protected static $generic = ['text', 'slug', 'html'];
 
-    public static function build($type, $value)
+    public static function build(Content $item, $fields)
     {
+        $fieldCollection = new FieldCollection([]);
 
-        switch (gettype($value)) {
-            case 'string':
-            case 'integer':
-                $type = new Generic($type, $value);
+        foreach ($fields as $label => $field) {
+            $type = $field['type'];
+            $data = $item->get($label);
+            
+            if (in_array($type, self::$generic)) {
+                $typeInstance = new Generic($label, $data);
+                $fieldCollection->add($typeInstance);
+            }
         }
+
+        foreach ($item->getTaxonomy() as $field) {
+            $type = $field->getTaxonomytype();
+            $slug = $field->getSlug();
+            $route = '/' . $type . '/' . $slug;
+            $attributes['taxonomy'][$type][$route] = $field->getName();
+        }
+
+        foreach ($item->getRelation() as $relation) {
+
+        }
+
+        return $fieldCollection;
     }
 }
